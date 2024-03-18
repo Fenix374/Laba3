bf0316d5229336021e9afec5a2564a1446beed08
Fenix374 <phoenilysenko@gmail.com>
2024-03-18 22:47:47 +0300

    Laba3

---
+++ Laba3/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using Laba3;
+using System;
+using System.Data.Entity;
+using System.Linq;
+using System.Windows;
+
+namespace Laba3
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            LoadData();
+        }
+
+        private void LoadData()
+        {
+                using (var context = new ASSASINS_CREEDEntities())
+                {
+                    context.GAMES.Load();
+                    context.CHARACTERS.Load();
+
+                    gamesDataGrid.ItemsSource = context.GAMES.Local.Select(g => new
+                    {
+                        Title = g.TITLE,
+                        RealeseDate = g.REALESEDATE,
+                        Platforms = g.PLATFORMS,
+                        Rating = g.RATING,
+                        Description = g.DESCRIPTIONn
+                    });
+
+                    charactersDataGrid.ItemsSource = context.CHARACTERS.Local.Select(c => new
+                    {
+                        Character = c.NAMEe,
+                        Game = c.GAMES,
+                        Description = c.DESCRIPTIONn
+                    });
+                }
+            
+            
+        }
+    }
+}
---
+++ Laba3/Window1.xaml.cs
@@ -0,0 +1,25 @@
+﻿using Laba3;
+using System;
+using System.Windows;
+
+namespace Laba3
+{
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+            LoadData();
+        }
+
+        private void LoadData()
+        {
+
+                var dataSet = new ASSASINS_CREEDDataSet1();
+
+                gamesDataGrid.ItemsSource = dataSet.GAMES;
+            charactersDataGrid.ItemsSource = dataSet.CHARACTERS;
+           
+        }
+    }
+}
